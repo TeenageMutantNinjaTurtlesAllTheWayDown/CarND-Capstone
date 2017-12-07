44a31b62d05345a968317a9033a86f26304130b0
Ilya Lyubimov <villytiger@gmail.com>
2017-12-07 23:23:36 +0300

    tl: Fix multithreading problems

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -2,6 +2,7 @@
 import sys
 from collections import namedtuple
 from math import pow, sqrt
+from threading import Lock
 
 import yaml
 
@@ -34,25 +35,33 @@ class Detector(object):
     def __init__(self):
         rospy.init_node('tl_detector')
 
-        self.car_index = 0
+        self.mutex = Lock()
+
+        self.car_index = None
+        self.state = TrafficLight.UNKNOWN
+        self.state_count = 0
+        self.last_stop_line = None
+
+        self.shared_car_index = None
         self.waypoints = None
         self.stop_lines = None
-
-        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
-        rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
         self.pub = rospy.Publisher('/traffic_waypoint', TrafficWaypoint,
                                    queue_size=1)
 
-        self.state = TrafficLight.UNKNOWN
-        self.state_count = 0
-        self.last_stop_line = None
+        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
+        rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
     def loop(self):
         rate = rospy.Rate(10)
 
         while not rospy.is_shutdown():
             rate.sleep()
+
+            with self.mutex:
+                if self.shared_car_index is None:
+                    continue
+                self.car_index = self.shared_car_index
 
             stop_line = self.get_stop_line()
             if stop_line is None:
@@ -75,39 +84,42 @@ class Detector(object):
             self.state_count += 1
 
     def pose_cb(self, msg):
-        if self.waypoints is None or self.stop_lines is None or len(self.stop_lines) == 0:
-            return
+        with self.mutex:
+            if self.waypoints is None:
+                return
 
-        if self.car_index > self.stop_lines[-1]:
-            self.car_index = 0
+            if self.shared_car_index is None or self.shared_car_index > self.stop_lines[-1]:
+                self.shared_car_index = 0
 
-        current_dist = distance(msg.pose.position,
-                                self.waypoints[self.car_index])
+            current_dist = distance(msg.pose.position,
+                                    self.waypoints[self.shared_car_index])
 
-        for i in range(self.car_index + 1, len(self.waypoints)):
-            dist = distance(msg.pose.position, self.waypoints[i])
-            if dist > current_dist:
-                break
-            current_dist = dist
-            self.car_index = i
+            for i in range(self.shared_car_index + 1, len(self.waypoints)):
+                dist = distance(msg.pose.position, self.waypoints[i])
+                if dist > current_dist:
+                    break
+                current_dist = dist
+                self.shared_car_index = i
 
     def waypoints_cb(self, msg):
-        self.waypoints = []
-        for waypoint in msg.waypoints:
-            position = waypoint.pose.pose.position
-            self.waypoints.append(Point(position.x, position.y))
+        with self.mutex:
+            config_string = rospy.get_param('/traffic_light_config')
+            stop_lines = yaml.load(config_string)['stop_line_positions']
 
-        config_string = rospy.get_param('/traffic_light_config')
+            if len(stop_lines) == 0:
+                return
 
-        rospy.loginfo('tl: got config_string: \n' + config_string)
+            self.waypoints = []
+            for waypoint in msg.waypoints:
+                position = waypoint.pose.pose.position
+                self.waypoints.append(Point(position.x, position.y))
 
-        self.stop_lines = []
-        for position in yaml.load(config_string)['stop_line_positions']:
-            index = self.get_closest_waypoint(Point(position[0], position[1]))
-            self.stop_lines.append(index)
-        self.stop_lines.sort()
-
-        rospy.loginfo('tl: loaded stop_lines: ' + str(self.stop_lines))
+            self.stop_lines = []
+            for position in stop_lines:
+                index = self.get_closest_waypoint(
+                    Point(position[0], position[1]))
+                self.stop_lines.append(index)
+            self.stop_lines.sort()
 
     def get_closest_waypoint(self, point):
         """Identifies the closest path waypoint to the given position
@@ -134,9 +146,6 @@ class Detector(object):
         return closest_index
 
     def get_stop_line(self):
-        if self.stop_lines is None:
-            return None
-
         for index in self.stop_lines:
             if index > self.car_index:
                 return index
@@ -146,13 +155,13 @@ class Detector(object):
 
 class ImageDetector(Detector):
     def __init__(self):
+        super(ImageDetector, self).__init__()
+
         variant = rospy.get_param('variant')
         config_string = rospy.get_param('/traffic_light_config')
         camera_info = yaml.load(config_string)['camera_info']
-        
-        super(ImageDetector, self).__init__()
 
-        self.camera_image = None
+        self.shared_camera_image = None
 
         self.bridge = CvBridge()
         self.light_classifier = TLClassifier(variant, camera_info['image_width'],
@@ -161,13 +170,16 @@ class ImageDetector(Detector):
         rospy.Subscriber('/image_color', Image, self.image_cb)
 
     def image_cb(self, msg):
-        self.camera_image = msg
+        with self.mutex:
+            self.shared_camera_image = msg
 
     def get_traffic_light_state(self):
-        if self.camera_image is None:
-            return TrafficLight.UNKNOWN
+        with self.mutex:
+            if self.shared_camera_image is None:
+                return TrafficLight.UNKNOWN
+            camera_image = self.shared_camera_image
 
-        image = self.bridge.imgmsg_to_cv2(self.camera_image, 'rgb8')
+        image = self.bridge.imgmsg_to_cv2(camera_image, 'rgb8')
 
         return self.light_classifier.get_classification(image)
 
@@ -176,49 +188,53 @@ class DummyDetector(Detector):
     def __init__(self):
         super(DummyDetector, self).__init__()
 
-        self.traffic_lights = None
+        self.shared_traffic_lights = None
         self.tl_map = {}
 
         rospy.Subscriber('/vehicle/traffic_lights',
                          TrafficLightArray, self.traffic_cb)
 
     def traffic_cb(self, msg):
-        if self.waypoints is None:
-            return
+        with self.mutex:
+            if self.waypoints is None:
+                return
 
-        if self.traffic_lights is None:
-            self.traffic_lights = {}
-            for light in msg.lights:
-                point = get_light_point(light)
-                index = self.get_closest_waypoint(light.pose.pose.position)
-                self.traffic_lights[point] = LightData(index, light.state)
-                self.tl_map[index] = point
-        else:
-            for light in msg.lights:
-                point = get_light_point(light)
-                index = self.traffic_lights[point].index
-                self.traffic_lights[point] = LightData(index, light.state)
+            if self.shared_traffic_lights is None:
+                self.shared_traffic_lights = {}
+                for light in msg.lights:
+                    point = get_light_point(light)
+                    index = self.get_closest_waypoint(light.pose.pose.position)
+                    self.shared_traffic_lights[point] = LightData(
+                        index, light.state)
+                    self.tl_map[index] = point
+            else:
+                for light in msg.lights:
+                    point = get_light_point(light)
+                    index = self.shared_traffic_lights[point].index
+                    self.shared_traffic_lights[point] = LightData(
+                        index, light.state)
 
     def get_traffic_light_state(self):
-        if self.traffic_lights is None:
-            return TrafficLight.UNKNOWN
+        with self.mutex:
+            if self.shared_traffic_lights is None:
+                return TrafficLight.UNKNOWN
 
-        tl_index = None
+            tl_index = None
 
-        for index in sorted(self.tl_map.keys()):
-            if index > self.car_index:
-                tl_index = index
-                break
+            for index in sorted(self.tl_map.keys()):
+                if index > self.car_index:
+                    tl_index = index
+                    break
 
-        if tl_index == None:
-            return TrafficLight.UNKNOWN
+            if tl_index == None:
+                return TrafficLight.UNKNOWN
 
-        dist = distance(self.waypoints[tl_index],
-                        self.waypoints[self.car_index])
-        if dist > 150:
-            return TrafficLight.UNKNOWN
+            dist = distance(self.waypoints[tl_index],
+                            self.waypoints[self.car_index])
+            if dist > 150:
+                return TrafficLight.UNKNOWN
 
-        return self.traffic_lights[self.tl_map[tl_index]].state
+            return self.shared_traffic_lights[self.tl_map[tl_index]].state
 
 
 if __name__ == '__main__':
